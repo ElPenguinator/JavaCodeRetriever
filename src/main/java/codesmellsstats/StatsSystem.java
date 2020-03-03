b7c3f41adf68112eb96442a4004182e3e96daea1
Prestat Dimitri <di.prestat@gmail.com>
2020-03-03 11:15:08 -0500

    Refactoring + HMU Fetching

--- src/main/java/codesmellsstats/StatsSystem.java
+++ src/main/java/codesmellsstats/StatsSystem.java
@@ -18,37 +18,41 @@ import java.util.List;
 import java.util.Optional;
 
 public class StatsSystem {
+    private static void fetchMethod(String path, String className, String methodName) throws IOException, URISyntaxException {
+        Path testFile = Paths.get(new URI("file:///" + path));
+        CompilationUnit compilationUnit = StaticJavaParser.parse(testFile);
+        //Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName("MainActivity");
+        Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName(className);
+        List<MethodDeclaration> methodToRetrieve = classA.get().getMethodsByName(methodName);
+        if (!methodToRetrieve.isEmpty())
+            System.out.println(methodToRetrieve.get(0).toString());
+    }
+
     public static void main(String args[]) throws URISyntaxException, IOException {
 
         if (args.length == 3) {
             //Path testFile = Paths.get(new URI("file:///D:/AndroidStudioProjects/androiddynamicverification/app/src/main/java/com/core/lambdaapplication/MainActivity.java"));
-            Path testFile = Paths.get(new URI("file:///"+args[0]));
-            CompilationUnit compilationUnit = StaticJavaParser.parse(testFile);
-            //Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName("MainActivity");
-            Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName(args[1]);
-            /*
             switch (args[2]) {
-                case "otm":
-                    List<MethodDeclaration> onTrimMemory = classA.get().getMethodsByName("onTrimMemory");
-                    if (!onTrimMemory.isEmpty())
-                        System.out.println(onTrimMemory.get(0).toString());
+                case "nlmr":
+                    fetchMethod(args[0], args[1], "onLowMemory");
                     break;
-                case "olm":
-                    List<MethodDeclaration> onLowMemory = classA.get().getMethodsByName("onLowMemory");
-                    if (!onLowMemory.isEmpty())
-                        System.out.println(onLowMemory.get(0).toString());
+                case "nltr":
+                    fetchMethod(args[0], args[1], "onTrimMemory");
+                    break;
+                case "hmu" :
+                    Path testFile = Paths.get(new URI("file:///" + args[0]));
+                    CompilationUnit compilationUnit = StaticJavaParser.parse(testFile);
+                    //Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName("MainActivity");
+                    Optional<ClassOrInterfaceDeclaration> classA = compilationUnit.getClassByName(args[1]);
+                    System.out.println(classA.toString());
                     break;
                 default:
-                    System.out.println("unrecognized option");
+                    System.out.println("unrecognized code smell");
                     break;
             }
-            */
-            List<MethodDeclaration> methodToRetrieve = classA.get().getMethodsByName(args[2]);
-            if (!methodToRetrieve.isEmpty())
-                System.out.println(methodToRetrieve.get(0).toString());
         }
         else {
-            System.out.println("Not enough arguments. Option 0 is the path, option 1 is the class name, option 2 is the name of the function");
+            System.out.println("Not enough arguments. Option 0 is the path, option 1 is the class name, option 2 is the name of the code smell");
         }
     }
 }
